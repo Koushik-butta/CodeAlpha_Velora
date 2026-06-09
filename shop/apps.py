01ce8c20e18a50afbf8214c37fc98458f3820085
Koushik <24211a0585@bvrit.ac.in>
2026-06-09 19:05:24 +0530

    fix: set database connection search_path to public schema dynamically

--- shop/apps.py
+++ shop/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
 
+@receiver(connection_created)
+def set_search_path(sender, connection, **kwargs):
+    if connection.vendor == 'postgresql':
+        with connection.cursor() as cursor:
+            cursor.execute("SET search_path TO public;")
 
 class ShopConfig(AppConfig):
     name = 'shop'
